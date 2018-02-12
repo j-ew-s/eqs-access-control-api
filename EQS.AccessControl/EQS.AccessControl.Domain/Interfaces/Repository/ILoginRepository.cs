ec5df7030f59896015689c19b150b82f3fdf68fe
j-ew-s <gabriel.scavassa@outlook.com>
2018-02-12 17:01:13 -0200

    Setting Domain : Adding Services and Repositoryintefaces. Setting Domain : Adding LoginService

---
+++ EQS.AccessControl/EQS.AccessControl.Domain/Interfaces/Repository/ILoginRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EQS.AccessControl.Domain.Entities;
+
+namespace EQS.AccessControl.Domain.Interfaces.Repository
+{
+    public interface ILoginRepository: IDisposable
+    {
+        Person Login(Credential credential);
+    }
+}
---
+++ EQS.AccessControl/EQS.AccessControl.Domain/Services/LoginService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using EQS.AccessControl.Domain.Entities;
+using EQS.AccessControl.Domain.Interfaces.Services;
+
+namespace EQS.AccessControl.Domain.Services
+{
+    public class LoginService : ILoginService
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Person Login(Credential credential)
+        {
+            //if(credential.Validations.IsValid)
+
+        }
+    }
+}
