9c824d299d3f43d9faff081057491e04367c44db
j-ew-s <gabriel.scavassa@outlook.com>
2018-02-12 17:32:13 -0200

    Adding EF Configuration for Credentials

---
+++ EQS.AccessControl/EQS.AccessControl.Repository/Configurations/CredentialConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using EQS.AccessControl.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EQS.AccessControl.Repository.Configurations
+{
+    class CredentialConfiguration : IEntityTypeConfiguration<Credential>
+    {
+        public void Configure(EntityTypeBuilder<Credential> builder)
+        {
+            builder.ToTable("Credentials");
+
+            builder.HasIndex(h => h.Id);
+
+            builder.HasOne(h => h.Person).WithOne(w => w.Credential).HasForeignKey<Person>(h => h.Id);
+
+            builder.Property(p => p.Password).IsRequired();
+
+            builder.Property(p => p.Username).IsRequired();
+
+            builder.Ignore(i => i.Validations);
+        }
+
+    }
+}
