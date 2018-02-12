fe9d78309b259b9314bb8b1cab2b43f17f28ea71
j-ew-s <gabriel.scavassa@outlook.com>
2018-02-12 17:35:18 -0200

    Setting EF For PersonRole

---
+++ EQS.AccessControl/EQS.AccessControl.Repository/Configurations/PersonRoleConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using EQS.AccessControl.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EQS.AccessControl.Repository.Configurations
+{
+    class PersonRoleConfiguration : IEntityTypeConfiguration<PersonRole>
+    {
+        public void Configure(EntityTypeBuilder<PersonRole> builder)
+        {
+            builder.ToTable("PersonRoles");
+
+            builder.HasKey(t => new { t.PersonId, t.RoleId });
+
+            builder.HasOne(o => o.Roles)
+                .WithMany(m => m.PersonRoles)
+                .HasForeignKey(h => h.RoleId);
+            builder.HasOne(o => o.Person)
+                .WithMany(m => m.PersonRoles)
+                .HasForeignKey(h => h.PersonId);
+        }
+    }
+
+    }
+}
