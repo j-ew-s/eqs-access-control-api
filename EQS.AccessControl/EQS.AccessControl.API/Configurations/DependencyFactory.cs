be9b6078f446cf516cead9e693170984e2177734
j-ew-s <gabriel.scavassa@outlook.com>
2018-05-22 08:36:28 +0100

    Adding Configuration

---
+++ EQS.AccessControl/EQS.AccessControl.API/Configurations/DependencyFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EQS.AccessControl.API.Configurations
+{
+    public class DependencyFactory
+    {
+    }
+}
