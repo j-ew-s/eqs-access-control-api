f6bee57c82210da365b270944743afb10638258d
j-ew-s <gabriel.scavassa@outlook.com>
2018-05-25 13:17:58 +0100

    adding RepositoryInitializer

---
+++ EQS.AccessControl/EQS.AccessControl.CrossCutting/RepositoryInitializer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EQS.AccessControl.CrossCutting
+{
+
+    public static class RepositoryInitializer 
+    {
+        
+    }
+}
